99713c8f1e57731c9de42d633d6420b2d3875011
francisxo999 <fra.verab@duocuc.cl>
2025-05-24 19:45:16 -0400

    creación logisticservice

---
+++ logisticaservice/src/main/java/com/perfulandia/logisticaservice/LogisticaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.logisticaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaserviceApplication.class, args);
+	}
+
+}
