a11e8eb761c858307da3207f3ef1c8c6d41b29bf
Mayckol2005 <may.mardones@duocuc.cl>
2025-05-24 23:37:18 -0400

    Desarrollo de microservicio Carrito(carritoItem)

---
+++ carritoservice/src/main/java/com/perfulandia/carritoservice/model/CarritoItem.java
@@ -0,0 +1,19 @@
+package com.perfulandia.carritoservice.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class CarritoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+}
