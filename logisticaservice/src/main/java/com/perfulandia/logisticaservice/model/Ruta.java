ddce1228b985cc44090771ce30c568c38f045c0c
francisxo999 <fra.verab@duocuc.cl>
2025-05-24 20:36:42 -0400

    clase Repartidor y Ruta creada en model

---
+++ logisticaservice/src/main/java/com/perfulandia/logisticaservice/model/Ruta.java
@@ -0,0 +1,22 @@
+package com.perfulandia.logisticaservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Ruta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String direccionEntrega;
+    private double distanciaKm;
+
+    private long repartidorId; // Relación con Usuario de otro microservicio
+}
